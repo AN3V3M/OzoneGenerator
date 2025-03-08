e40d97673647859b00c5c787557d242b78be2f96
AN3V3M <144987817+AN3V3M@users.noreply.github.com>
2025-03-08 19:31:38 +0100

    Add files via upload

---
+++ OzoneGenerator/OzoneGenerator/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OzoneGenerator
+{
+    public partial class MainWindow : Window
+    {
+        private OzoneGrid ozoneGrid;
+        private const int GridSize = 27;
+        private Button[,] buttons = new Button[GridSize, GridSize];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ozoneGrid = new OzoneGrid();
+            InitializeGrid();
+            UpdateGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            for (int i = 0; i < GridSize; i++)
+            {
+                for (int j = 0; j < GridSize; j++)
+                {
+                    Button btn = new Button
+                    {
+                        Width = 20,
+                        Height = 20,
+                        Margin = new Thickness(1)
+                    };
+                    btn.Click += (s, e) => Cell_Click(i, j);
+                    buttons[i, j] = btn;
+                    OzoneGrid.Children.Add(btn);
+                }
+            }
+        }
+
+        private void UpdateGrid()
+        {
+            for (int i = 0; i < GridSize; i++)
+            {
+                for (int j = 0; j < GridSize; j++)
+                {
+                    int value = ozoneGrid.GetParcel(i, j);
+                    buttons[i, j].Content = value > 0 ? value.ToString() : "";
+
+                    if (value == -1)
+                        buttons[i, j].Background = Brushes.Gray;
+                    else if (value == 0)
+                        buttons[i, j].Background = Brushes.White;
+                    else
+                        buttons[i, j].Background = Brushes.Blue;
+                }
+            }
+        }
+
+        private void Cell_Click(int row, int col)
+        {
+            ozoneGrid.PointClean(row, col);
+            UpdateGrid();
+        }
+
+        private void NextDayButton_Click(object sender, RoutedEventArgs e)
+        {
+            ozoneGrid.OzoneClean(0, 0);
+            UpdateGrid();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ozoneGrid.Restart();
+            UpdateGrid();
+        }
+    }
+}
---
+++ OzoneGenerator/OzoneGenerator/OzoneGrid.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace OzoneGenerator
+{
+    public class OzoneGrid
+    {
+        private const int Size = 27;
+        private int[,] grid = new int[Size, Size];
+        private Random random = new Random();
+
+        public OzoneGrid()
+        {
+            Restart();
+        }
+
+        public int GetParcel(int row, int column)
+        {
+            return grid[row, column];
+        }
+
+        public void OzoneClean(int row, int column)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (grid[i, j] > 0) grid[i, j]--;
+                }
+            }
+
+            int randRow, randCol;
+            do
+            {
+                randRow = random.Next(1, Size - 1);
+                randCol = random.Next(1, Size - 1);
+            } while (grid[randRow, randCol] != 0);
+
+            ApplyOzone(randRow, randCol);
+        }
+
+        public void PointClean(int row, int column)
+        {
+            if (grid[row, column] != -1)
+                ApplyOzone(row, column);
+        }
+
+        private void ApplyOzone(int row, int column)
+        {
+            grid[row, column] = 5;
+            int[] dr = { -1, 1, 0, 0 };
+            int[] dc = { 0, 0, -1, 1 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newRow = row + dr[i];
+                int newCol = column + dc[i];
+
+                if (newRow >= 1 && newRow < Size - 1 && newCol >= 1 && newCol < Size - 1 && grid[newRow, newCol] != -1)
+                {
+                    grid[newRow, newCol] = 4;
+                }
+            }
+        }
+
+        public void Restart()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (i == 0 || i == Size - 1 || j == 0 || j == Size - 1)
+                        grid[i, j] = -1; 
+                    else
+                        grid[i, j] = 0;
+                }
+            }
+        }
+    }
+}
